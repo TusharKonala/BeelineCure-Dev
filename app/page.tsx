0be79f84827b776ed406ab1ae153e69e0eb0dd79
Tushar Konala <tusharkonala284@gmail.com>
2026-02-24 15:57:28 +0530

    feat: add hero carousel with video backgrounds and responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/components/Navbar";
+import { HeroCarousel } from "@/components/HeroCarousel";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 dark:bg-black">
       <Navbar />
+      <HeroCarousel />
     </div>
   );
 }
