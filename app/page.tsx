7efab381ba00bb15c10fddb0b862a87ec5d8fe3b
Tushar Konala <tusharkonala284@gmail.com>
2026-02-24 17:40:29 +0530

    feat: add features section with font setup and custom text utilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import { HeroCarousel } from "@/components/HeroCarousel";
+import { FeaturesSection } from "@/components/FeaturesSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 dark:bg-black">
       <Navbar />
       <HeroCarousel />
+      <FeaturesSection />
     </div>
   );
 }
